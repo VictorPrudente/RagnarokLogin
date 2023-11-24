87457a922d50431a8e08b6b9398e137e45ac32c3
Victor Prudente <victorprudt@gmail.com>
2023-11-24 13:51:35 -0300

    First Commit

---
+++ src/main/java/com/ragnaroklogin/RagnarokLoginApplication.java
@@ -0,0 +1,13 @@
+package com.ragnaroklogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagnarokLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagnarokLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ragnaroklogin/RagnarokLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ragnaroklogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagnarokLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
